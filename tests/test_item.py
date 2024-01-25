9ff0eefdcb8aff7043b5d78e82d7ba8715f233fb
marinam0406 <marinam0406@yandex.ru>
2024-01-25 19:21:53 +0300

    Fix test_item.py

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,19 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+@pytest.fixture
+def test_item():
+    return Item("Смартфон", 10000, 2)
+
+def test_calculate_total_price(test_item):
+    assert test_item.calculate_total_price() == 20000
+    assert test_item.price == 10000
+
+def test_apply_discount(test_item):
+    test_item.pay_rate = 0.8
+    test_item.apply_discount()
+    assert test_item.calculate_total_price() == 16000.0
+    assert test_item.price == 8000
+
